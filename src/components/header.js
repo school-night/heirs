401c2d2439fd6d92cc063a8abc117c48914c3f68
Zeke Wattles <zekewattles@gmail.com>
2020-08-06 21:04:39 -0700

    added logo to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,13 @@
 import React from "react"
 import { Link } from "gatsby"
+import Logo from "../assets/heirs-logo-white.svg"
 
 export default function Home() {
   return (
     <div>
-      <Link to="/">Heirs Logo</Link>
+      <Link to="/">
+        <img src={Logo} alt="HEIRS" height="100" />
+      </Link>
     </div>
   )
 }
