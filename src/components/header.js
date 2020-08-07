8a575e18a5d149d078be2044143db10867c8aab6
Ali Torbati <ali.torbati@gmail.com>
2020-08-06 21:24:51 -0700

    add some basic styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,19 @@
 import React from "react"
 import { Link } from "gatsby"
+import Nav from "../components/nav"
 import Logo from "../assets/heirs-logo-white.svg"
 
 export default function Home() {
   return (
-    <div>
-      <Link to="/">
-        <img src={Logo} alt="HEIRS" height="100" />
-      </Link>
+    <div className="header">
+      <div className="nav-area">
+        <Nav />
+      </div>
+      <div className="logo-area">
+        <Link to="/">
+          <img src={Logo} alt="HEIRS" height="100" />
+        </Link>
+      </div>
     </div>
   )
 }
