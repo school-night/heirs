90bb059bf2099162e044272e53ea16fd7c4cfc8e
Ali Torbati <ali.torbati@gmail.com>
2020-08-13 17:54:13 -0700

    add video playsinline

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict"
 ;(function () {
   const isMobile = (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
-  // console.log({isMobile})
+  alert(isMobile)
 
 
   const videoTriggers = [
